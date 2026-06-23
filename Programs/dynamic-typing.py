a5d5c009144a67de736cbff3263bc7260c439bb5
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-06-23 18:49:35 +0530

    Add dynamic typing demo in Python
    Demonstrates dynamic typing in Python by showing how a variable can change its type without explicit declaration.

---
+++ Programs/dynamic-typing.py
@@ -0,0 +1,41 @@
+############################################################################################################################
+#
+# Program      : Dynamic Typing Demo
+# Input        : 5 , 5.5 , Python
+# Output       : <class 'int'> , <class 'float'> , <class 'str'>
+# Description  : Demonstrates dynamic typing in Python.
+# Author       : Samruddh Shivkumar Birajdar
+#
+############################################################################################################################
+
+a = 5
+print(type(a))
+
+a = 5.5
+print(type(a))
+
+a = "Python"
+print(type(a))
+
+# Output:
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+
+'''
+Feature Demonstrated: Dynamic Typing
+
+The same variable can refer to different types of values.
+
+a → int
+a → float
+a → string
+
+No explicit declaration is required.
+'''
+
+###############################################################################################################################
+#
+# Application : Clarifies that Python variables can change type dynamically without explicit declaration.
+#
+################################################################################################################################
