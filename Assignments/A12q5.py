212efa79ec17246975845823b6003968b3e7f9c7
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-07-05 16:33:55 +0530

    Add program to print N numbers in reverse order
    This program accepts a number and prints that many numbers in reverse order, demonstrating the concept of loops.

---
+++ Assignments/A12q5.py
@@ -0,0 +1,21 @@
+############################################################################################################################
+#
+# Program      : Print N Numbers in Reverse Demo
+# Input        : 5
+# Output       : 5 4 3 2 1
+# Description  : Accepts one number and prints that many numbers in reverse order.
+# Author       : Samruddh Shivkumar Birajdar
+#
+############################################################################################################################
+
+def PrintReverse(n):
+    for i in range(n, 0, -1):
+        print(i, end=" ")
+
+PrintReverse(5)
+
+###############################################################################################################################
+#
+# Application : Demonstrates how to print numbers in reverse order starting from N.
+#
+################################################################################################################################
