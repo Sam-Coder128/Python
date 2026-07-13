46a0bc59ec8eaf2a5aa5ca8b8f012da0022416ef
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-07-13 15:32:26 +0530

    Add number sign check demo program
    This program checks whether a number is positive, negative, or zero and prints the corresponding message.

---
+++ Assignments/A16q6.py
@@ -0,0 +1,27 @@
+############################################################################################################################
+#
+# Program      : Number Sign Check Demo
+# Input        : 11 , -8 , 0
+# Output       : Positive Number , Negative Number , Zero
+# Description  : Checks whether a number is positive, negative or zero.
+# Author       : Samruddh Shivkumar Birajdar
+#
+############################################################################################################################
+
+def ChkSign(n):
+    if n > 0:
+        print("Positive Number")
+    elif n < 0:
+        print("Negative Number")
+    else:
+        print("Zero")
+
+ChkSign(11)
+ChkSign(-8)
+ChkSign(0)
+
+###############################################################################################################################
+#
+# Application : Demonstrates how to check sign of a number.
+#
+################################################################################################################################
