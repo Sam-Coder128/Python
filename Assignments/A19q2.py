0f2bcb44b2b2fb74a1594565ea09b73a3c98a41c
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-07-17 17:24:22 +0530

    Add lambda multiplication demo program
    This program demonstrates a lambda function that multiplies two numbers and prints the results.

---
+++ Assignments/A19q2.py
@@ -0,0 +1,20 @@
+############################################################################################################################
+#
+# Program      : Lambda Multiplication Demo
+# Input        : 4 3 , 6 3
+# Output       : 12 , 18
+# Description  : Lambda function which accepts two parameters and returns multiplication.
+# Author       : Samruddh Shivkumar Birajdar
+#
+############################################################################################################################
+
+multiply = lambda a, b: a * b
+
+print(multiply(4, 3))
+print(multiply(6, 3))
+
+###############################################################################################################################
+#
+# Application : Demonstrates lambda function for multiplication of two numbers.
+#
+################################################################################################################################
