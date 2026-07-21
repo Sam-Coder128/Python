1b64c2f39a08b0208aa6d2fa62c56ace132495dd
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-07-21 15:59:36 +0530

    Add Sum and Product Threads Demo
    This program demonstrates the use of threads to compute the sum and product of a list of integers. It creates two threads, one for calculating the sum and another for the product, and prints the results.

---
+++ Assignments/A21q4.py
@@ -0,0 +1,37 @@
+############################################################################################################################
+#
+# Program      : Sum and Product Threads Demo
+# Functions    : SumThread(), ProductThread()
+# Input        : [1, 2, 3, 4]
+# Output       : Sum = 10 , Product = 24
+# Description  : Creates two threads to compute sum and product of list elements.
+# Author       : Samruddh Shivkumar Birajdar
+#
+############################################################################################################################
+
+import threading
+
+results = {}
+
+def SumThread(lst):
+    results['sum'] = sum(lst)
+
+def ProductThread(lst):
+    product = 1
+    for x in lst:
+        product *= x
+    results['product'] = product
+
+data = [1, 2, 3, 4]
+
+t1 = threading.Thread(target=SumThread, args=(data,), name="SumThread")
+t2 = threading.Thread(target=ProductThread, args=(data,), name="ProductThread")
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print("Sum:", results['sum'])
+print("Product:", results['product'])
