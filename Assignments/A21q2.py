0d3589c0d4ba39f4be6815e18c4cc40e7890a75c
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-07-21 15:58:47 +0530

    Add Max-Min Threads Demo program
    This program creates two threads to calculate and print the maximum and minimum elements from a given list.

---
+++ Assignments/A21q2.py
@@ -0,0 +1,29 @@
+############################################################################################################################
+#
+# Program      : Max-Min Threads Demo
+# Functions    : MaxThread(), MinThread()
+# Input        : [10, 25, 5, 40, 7]
+# Output       : Maximum = 40 , Minimum = 5
+# Description  : Creates two threads to calculate maximum and minimum element from a list.
+# Author       : Samruddh Shivkumar Birajdar
+#
+############################################################################################################################
+
+import threading
+
+def MaxThread(lst):
+    print("Maximum element:", max(lst))
+
+def MinThread(lst):
+    print("Minimum element:", min(lst))
+
+data = [10, 25, 5, 40, 7]
+
+t1 = threading.Thread(target=MaxThread, args=(data,), name="MaxThread")
+t2 = threading.Thread(target=MinThread, args=(data,), name="MinThread")
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
