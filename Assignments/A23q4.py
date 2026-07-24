fa94dc6131f17b63ab66ada5d1cd2ef2b3e22a40
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-07-24 14:57:29 +0530

    Add Pool Map Odd Count Demo program
    This program utilizes multiprocessing to count odd numbers up to given inputs.

---
+++ Assignments/A23q4.py
@@ -0,0 +1,26 @@
+############################################################################################################################
+#
+# Program      : Pool Map Odd Count Demo
+# Functions    : count_odd(), main()
+# Input        : [1000000,2000000,3000000,4000000]
+# Output       : Process ID, Input Number, Odd Number Count
+# Description  : Uses Pool.map() to count odd numbers between 1 and N.
+# Author       : Samruddh Shivkumar Birajdar
+#
+############################################################################################################################
+
+from multiprocessing import Pool
+import os
+
+def count_odd(n):
+    return (os.getpid(), n, (n+1)//2)
+
+def main():
+    data = [1000000,2000000,3000000,4000000]
+    with Pool() as p:
+        result = p.map(count_odd, data)
+    for pid, num, count in result:
+        print("Process ID:", pid, "Input Number:", num, "Odd Number Count:", count)
+
+if __name__ == "__main__":
+    main()
