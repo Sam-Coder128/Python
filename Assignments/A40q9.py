b96829f485c16c86fbe090922e343392ac43b344
Samruddh Birajdar <samruddh.birajdar24@gmail.com>
2026-08-16 12:39:54 +0530

    Add PerformanceIndex feature and retrain model
    This program adds a new feature 'PerformanceIndex' to the dataset and retrains a decision tree model to evaluate its accuracy.

---
+++ Assignments/A40q9.py
@@ -0,0 +1,32 @@
+############################################################################################################################
+#
+# Program      : Add PerformanceIndex Feature
+# Functions    : add_feature_and_train(), main()
+# Input        : student_performance_ml.csv
+# Output       : Accuracy with new feature
+# Description  : Adds PerformanceIndex = (StudyHours*2)+Attendance and retrains model.
+# Author       : Samruddh Shivkumar Birajdar
+#
+############################################################################################################################
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+def add_feature_and_train(filename):
+    df = pd.read_csv(filename)
+    df['PerformanceIndex'] = (df['StudyHours']*2) + df['Attendance']
+    X = df.drop('FinalResult', axis=1)
+    y = df['FinalResult']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = DecisionTreeClassifier(random_state=42)
+    model.fit(X_train, y_train)
+    acc = accuracy_score(y_test, model.predict(X_test)) * 100
+    print("Accuracy with PerformanceIndex feature:", acc, "%")
+
+def main():
+    add_feature_and_train("student_performance_ml.csv")
+
+if __name__ == "__main__":
+    main()
